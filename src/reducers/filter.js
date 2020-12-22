460a97dff404b5e0beb0043d15c92caf49b1fbc1
Евгений Жиров <girov189916@gmail.com>
2020-12-22 16:48:58 +0300

    filters

--- src/reducers/filter.js
+++ src/reducers/filter.js
@@ -1,7 +1,7 @@
 import {CHANGE_DATE,CHANGE_COUNT_ROW,CHANGE_REFRESH_DATA} from '../constants/actions'
 import {filter_value,countRowTable,delay_refresh_data} from '../constants/filters_values'
 
-const initialState = {start: filter_value.end, end: filter_value.end,countRowTable:countRowTable,delay_refresh_data:delay_refresh_data}
+const initialState = {start: filter_value.start, end: filter_value.end,countRowTable:countRowTable,delay_refresh_data:delay_refresh_data}
 
 export const filter = (state=initialState, action)=>{
   console.log(action)
